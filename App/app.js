84211eff4df0b902b2dbe2e29a1d13c8997c84c5
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-04 01:28:34 +0000

    Mar 3, 2023, 8:28 PM

---
+++ App/app.js
@@ -0,0 +1,6 @@
+let appTitle = document.createElement("h1")
+let appTitleText = document.createTextNode("Procrastination Pie")
+appTitle.appendChild(appTitleText)
+
+let divContainer = document.getElementById("containerDiv")
+divContainer.appendChild(appTitle)
