b9ddfa4c783be5b17d3b1cdfde2e7fe80eb33a3d
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-04 01:41:52 +0000

    Mar 3, 2023, 8:41 PM

--- App/app.js
+++ App/app.js
@@ -4,3 +4,7 @@ appTitle.appendChild(appTitleText)
 
 let divContainer = document.getElementById("containerDiv")
 divContainer.appendChild(appTitle)
+
+let defaultPie = document.createElement("img")
+defaultPie.setAttribute("src", "./assets/0.png")
+divContainer.appendChild(defaultPie)
