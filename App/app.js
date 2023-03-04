ccbbbacdeb88c3efb80a92823d0fc33a20acf0c9
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-04 02:34:18 +0000

    Mar 3, 2023, 9:34 PM

--- App/app.js
+++ App/app.js
@@ -1,10 +1,11 @@
+/*heading for title of app*/
 let appTitle = document.createElement("h1")
 let appTitleText = document.createTextNode("Procrastination Pie")
 appTitle.appendChild(appTitleText)
-
+/*div*/
 let divContainer = document.getElementById("containerDiv")
 divContainer.appendChild(appTitle)
-
+/*default pie image*/
 let defaultPie = document.createElement("img")
 defaultPie.setAttribute("src", "./assets/0.png")
 defaultPie.setAttribute("width", "500px")
@@ -13,18 +14,18 @@ divContainer.appendChild(defaultPie)
 
 let lineBreak = document.createElement("br")
 divContainer.appendChild(lineBreak)
-
+/*button to add slice*/
 let addSliceButton = document.createElement("button")
 addSliceButton.setAttribute("id", "addButton")
 addSliceButton.setAttribute("type", "button")
 let addSliceButtonText = document.createTextNode("ADD")
 addSliceButton.appendChild(addSliceButtonText)
 divContainer.appendChild(addSliceButton)
-
+/*function to add slice*/
 function addSlice() {
     console.log("Added slice")
 }
-
+/*initial function*/
 function init() {
     /*button clicks to add slices*/
     let addSliceButton = document.getElementById("addButton")
