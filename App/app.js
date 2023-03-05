ef24dec88aa81e31266605425421f429b7054f73
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-05 04:17:23 +0000

    Mar 4, 2023, 11:17 PM

--- App/app.js
+++ App/app.js
@@ -15,6 +15,13 @@ let defaultGem = document.createElement("img")
 defaultGem.setAttribute("src", "./assets/gems/odd/7/7_7.png")
 defaultGem.setAttribute("class", "gems")
 divContainer.appendChild(defaultGem)
+/*button to start new project*/
+let newProjectButton = document.createElement("button")
+newProjectButton.setAttribute("id", "newProject")
+newProjectButton.setAttribute("type", "button")
+let newProjectButtonText = documement.createTextNode("New Project")
+newProjectButton.appendChild(newProjectButtonText)
+divContainer.appendChild(newProjectButton)
 /*button to add slice*/
 let addSliceButton = document.createElement("button")
 addSliceButton.setAttribute("id", "addButton")
@@ -22,6 +29,10 @@ addSliceButton.setAttribute("type", "button")
 let addSliceButtonText = document.createTextNode("ADD")
 addSliceButton.appendChild(addSliceButtonText)
 divContainer.appendChild(addSliceButton)
+/*function to activate popup window*/
+function openPop() {
+    window.open("popup.html", "Popup", "width=400, height=300")
+}
 /*function to add slice*/
 function addSlice() {
     const frameImg = document.getElementById("frames")
@@ -47,7 +58,9 @@ function addSlice() {
 }
 /*initial function*/
 function init() {
-    /*prompt the user for a task name*/
+    /*prompt the user for a task name, numbers of tasks, and days until deadline*/
+    let newProjectButton = document.getElementById("newProject")
+    newProjectButton.addEventListener("click", openPop)
     /*button clicks to add slices*/
     let addSliceButton = document.getElementById("addButton")
     addSliceButton.addEventListener("click", addSlice)
