5027458ae3c266b85857cae87f75011f34505e62
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-05 18:48:50 +0000

    Mar 5, 2023, 1:48 PM

--- App/app.js
+++ App/app.js
@@ -11,8 +11,8 @@ childDiv.setAttribute("id", "childDiv")
 divContainer.appendChild(childDiv)
 /*DOMContentLoaded*/
 document.addEventListener("DOMContentLoaded", function(event) {
-    let pnameOutput = localStorage.getItem("pnameOutput")
-    if (pnameOutput !== null) {
+    let outputHTML = localStorage.getItem("pnameOutput")
+    if (outputHTML !== null) {
         let outputElement = document.getElementById("childDiv")
         let outputTextNode = document.createTextNode(outputHTML)
         outputElement.appendChild(outputTextNode)
