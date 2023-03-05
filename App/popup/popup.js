add36a82a54db6ea133fa0618033af9d1561b5eb
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-05 04:39:51 +0000

    Mar 4, 2023, 11:39 PM

--- App/popup/popup.js
+++ App/popup/popup.js
@@ -2,9 +2,17 @@ function cancelNewProject() {
     window.close();
 }
 
+function activatenewProject() {
+    window.close()
+}
+
 function init() {
+/*cancel project and close window*/
 let cancelButton = document.getElementById("cancel")
 cancelButton.addEventListener("click", cancelNewProject)
+/*activate new project and close window*/
+let okButton = document.getElementById("ok")
+okButton.addEventListener("click", activatenewProject)
 }
 
 window.addEventListener("load", init, false);
