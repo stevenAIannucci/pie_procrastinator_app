3ca6c80dfa7246199f336d697dbe2300b8a22055
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-06 03:30:07 +0000

    Mar 5, 2023, 10:30 PM

--- App/app.js
+++ App/app.js
@@ -71,7 +71,7 @@ if (taskNumber === 1) {
 function addSlice() {
     const frameImg = document.getElementById("frames")
     if (frameImg.src.endsWith("/assets/frames/odd/1/0.png")) {
-        frameImg.src = "/assets/frames/even/8/1.png"
+        frameImg.src = "./assets/frames/odd/1/1.png"
     }
 
     if (frameImg.src.endsWith("/assets/frames/even/8/0.png")) {
