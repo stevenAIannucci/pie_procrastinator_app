4ca6754a0148c729336b53f2a3c11f7a6e8992f4
stevenAIannucci <89707282+stevenAIannucci@users.noreply.github.com>
2023-03-06 05:46:44 +0000

    Mar 6, 2023, 12:46 AM

--- App/tasklist/tasklist.js
+++ App/tasklist/tasklist.js
@@ -11,5 +11,5 @@ if (taskNumber === 8) {
     task1Input.setAttribute("type", "text")
     task1Input.setAttribute("id", "task1Input")
     task1Input.setAttribute("name", "task1Input")
-    tasklist.appendChild(task1Text)
+    tasklist.appendChild(task1Input)
 }
